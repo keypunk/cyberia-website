8a1161c9e36ab3abbdedb34f909efe11469e3b08
Cyrus Salehi <cyrussalehi@MacBook-Pro-von-Cyrus.local>
2024-06-17 23:48:17 +0200

    Add states to login and register component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+  const [data, setData] = useState({
+    email: '',
+    password: ''
+  })
+
+
+  const loginUser = (event) => {
+    event.preventDefault()
+  }
+
+  const handleEmailChange = (event) => {
+    setData({...data, email: event.target.value})
+  }
+   
   return (
-    <div>Login</div>
+    <div>
+      <form onSubmit={loginUser}>
+        <label>Email</label>
+        <input type='email' placeholder='enter email...' />
+        <label>Password</label>
+        <input type='password' placeholder='enter password...' />
+        <button type="submit">Login</button>
+      </form>
+    </div>
   )
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Register = () => {
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+
+  const registerUser = (event) => {
+    event.preventDefault()
+  }
+
+  const handleNameChange = (event) => {
+    setData({...data, name: event.target.value})
+  }
+
+  const handleEmailChange = (event) => {
+    setData({...data, email: event.target.value})
+  }
+
+  const handlePasswordChange = (event) => {
+    setData({...data, password: event.target.value})
+  }
+  
   return (
-    <div>Register</div>
+    <div>
+      <form onSubmit={registerUser}>
+        <label>Name</label>
+        <input type='text' placeholder='enter name...' value={data.name} onChange={handleNameChange} />
+        <label>Name</label>
+        <input type='email' placeholder='enter email...' value={data.email} onChange={handleEmailChange} />
+        <label>Name</label>
+        <input type='password' placeholder='enter password...' value={data.password} onChange={handlePasswordChange} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
-}
+
+
 
 export default Register
