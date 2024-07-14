af03cf2a2fc79e42862a31f39b2156ded05f70b5
keypunk <salehiharun@gmail.com>
2024-07-14 20:27:18 +0200

    Add product listing through backend calls
    Before we used a local file on the frontend side to get the product data.
    Since our backend handles product persistence, we want to have the data
    on backend side. Add server code on backend to listen to calls to
    products.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,21 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
 import Product from './pages/Product'
 import { Toaster } from 'react-hot-toast'
-import { UserContextProvider } from '../context/userContext'
 
 const App = () => {
   return (
-    <UserContextProvider>
+    <>
       <Navbar />
       <Toaster position='bottom-right' toastOptions={{duration: 2000}} />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/product/:slug' element={<Product />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+	<Route path='/' element={<Home />} />
+	<Route path='/register' element={<Register />} />
+	<Route path='/login' element={<Login />} />
+	<Route path='/product/:slug' element={<Product />} />
       </Routes>
-    </UserContextProvider>
+    </>
   )
 }
 
